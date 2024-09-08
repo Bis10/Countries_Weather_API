0db3d94e785038e273b498aea47c878370591b2c
Suyel Bishnu <suyel2007@gmail.com>
2024-09-08 21:35:00 +0300

    Add new routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Layout from './pages/Layout'
+import ErrorPage from './components/ErrorPage'
+import Countries from './components/Countries'
+import Home from './components/Home'
 
 
 function App() {
   
 
   return (
-    <>
-      My app will be here
-      
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route>
+            {/* This is where other routes will go to allow layout to be visible */}
+            <Route path="/" element={<Home />} />
+            <Route path="/countries" element={<Countries />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
