cbef8ef850eacaa60a7b89f794429c7c3c0a3723
Suyel Bishnu <suyel2007@gmail.com>
2024-09-13 14:52:26 +0300

    Create favourite slice

---
+++ src/store/favouritesSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  favourites:[],
+};
+
+export const favouritesSlice = createSlice({
+  name: "countries",
+  initialState,
+  reducers: {
+    addFavourite(state, action){
+        state.favourites = [...state.favourites, action.payload]
+    },
+    clearFavourites(state){
+        state.favourites = []
+    },
+    removeFavourite(state, action){
+        state.favourites = state.favourites.filter(favourite=>favourite !== action.payload)
+    }
+  },
+});
+export const { addFavourite, clearFavourites, removeFavourite } = favouritesSlice.actions;
+export default favouritesSlice.reducer;
