e7b8816ebc044a077a319f0d89a1635a2aef2745
Suyel Bishnu <suyel2007@gmail.com>
2024-09-13 14:52:54 +0300

    Config store with favourite slice

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import countriesReducer from "./countriesSlice";
+import favouritesReducer from "./favouritesSlice";
 
 
 export const store = configureStore({
   reducer: {
     countries: countriesReducer,
+    favourites: favouritesReducer,
   },
 });
