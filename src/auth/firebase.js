72336241489b8b88cc1fde317f70c04dc4fee1c3
Suyel Bishnu <suyel2007@gmail.com>
2024-09-26 22:02:43 +0300

    Add, remove & clear favourites from firebase

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -1,9 +1,19 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut} from "firebase/auth";
-import {addDoc, collection, getFirestore} from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  getDocs,
+  getFirestore,
+  query,
+  where,
+} from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,41 +25,93 @@ const firebaseConfig = {
   appId: "1:689684782081:web:0a86c121fc7f3985027400",
 };
 
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 const db = getFirestore(app);
 
+const loginWithEmailAndPassword = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (error) {
+    console.log(error);
+    alert(error.message);
+  }
+};
 
+const registerWithEmailAndPassword = async (name, email, password) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+    await addDoc(collection(db, "users"), {
+      uid: user.uid,
+      name,
+      authProvider: "local",
+      email,
+    });
+  } catch (error) {
+    console.log(error);
+    alert(error.message);
+  }
+};
 
-const loginWithEmailAndPassword = async (email, password)=>{
-    try {
-        await signInWithEmailAndPassword(auth, email, password)
-    } catch (error) {
-        console.log(error);
-        alert(error.message);
+const logout = () => {
+  signOut(auth);
+};
+
+const addFavouriteToFirebase = async (uid, name) => {
+  try {
+    await addDoc(collection(db, `users/${uid}/favourites`), { name });
+    console.log("Favourite added to Firebase");
+  } catch (error) {
+    console.log("Error removing favourite from firebase", error);
+  }
+};
+
+const removeFavouriteFromFirebase = async (uid, name) => {
+  try {
+    if (!name) {
+      console.error(
+        "Error removing favourite from firebase: Name parameter undefined"
+      );
+      return;
     }
+    const q = query(
+      collection(db, `users/${uid}/favourites`),
+      where("name", "==", name)
+    );
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      deleteDoc(doc.ref);
+      console.log("Favourite removed from Firebase");
+    });
+  } catch (error) {
+    console.log("Error removing favourite from firebase", error);
+  }
 };
 
-const registerWithEmailAndPassword = async(name, email, password)=>{
-    try {
-        const res = await createUserWithEmailAndPassword(auth, email, password);
-        const user = res.user;
-        await addDoc(collection(db, "users"),{
-            uid: user.uid,
-            name,
-            authProvider: "local",
-            email,
-        })
-    } catch (error) {
-        console.log(error);
-        alert(error.message);
-    }
+const clearFavouritesFromFirebase = async (uid) => {
+  try {
+    const q = query(collection(db, `users/${uid}/favourites`));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      deleteDoc(doc.ref);
+      console.log("Favourites cleared from Firebase");
+    });
+  } catch (error) {
+    console.log("Error clearing favourites from firebase", error);
+  }
 };
 
-const logout = ()=>{
-    signOut(auth);
+export {
+  addFavouriteToFirebase,
+  auth,
+  clearFavouritesFromFirebase,
+  db,
+  loginWithEmailAndPassword,
+  logout,
+  registerWithEmailAndPassword,
+  removeFavouriteFromFirebase,
 };
-
-export {auth, db, loginWithEmailAndPassword, registerWithEmailAndPassword, logout}; // Export the auth and db objects
