4a4d31f67f35e2508a3e096a90559e2545f672ad
Suyel Bishnu <suyel2007@gmail.com>
2024-09-26 22:04:33 +0300

    Add props to the country single

--- src/components/CountrySingle.jsx
+++ src/components/CountrySingle.jsx
@@ -1,20 +1,15 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
-import { useLocation, useNavigate } from "react-router-dom";
-import { initializeCountries } from "../services/countriesServices";
 import axios from "axios";
-import { Button, Col, Container, Row, Spinner, Image } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { Button, Col, Container, Image, Row, Spinner } from "react-bootstrap";
+import { useLocation, useNavigate } from "react-router-dom";
 
-const CountrySingle = () => {
+const CountrySingle = (props) => {
   const location = useLocation();
-  const navigate = useNavigate();
-
-  const dispatch = useDispatch();
-
-  // const country = location.state?.country;
-  const country = location.state.country;
+  const country = props.country || location.state.country;
   const [weather, setWeather] = useState("");
   const [isWeatherLoading, setIsWeatherLoading] = useState(true);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios
@@ -23,72 +18,60 @@ const CountrySingle = () => {
           country.capital
         }&units=metric&appid=${import.meta.env.VITE_WEATHER_API_KEY}`
       )
+      .catch((error) => {
+        console.log(error);
+      })
       .then((response) => {
         setWeather(response.data);
-        setIsWeatherLoading(false);
-      })
-      .catch((error) => {
-        console.error(error);
         setIsWeatherLoading(false);
       });
   }, [country.capital]);
 
-  console.log("Weather:", weather);
-
-  // Handle the loading case first
+  console.log("Weather: ", weather);
 
   if (isWeatherLoading) {
-    // Create a spinner
-    return (<Col className="text-center m-5">
-      <Spinner
-        animation="border"
-        role="status"
-        className="center"
-        variant="info"
-      >
-        <span className="visually-hidden">Loading...</span>
-      </Spinner>
-    </Col>
-    );
-  }
     return (
-      <Container fluid>
-        <Row>
-          <Col className="mt-5 d-flex justify-context-center">
-            <Image src={country.flags.svg} />
-          </Col>
-          <Col>
-            <h2>{country.name.common}</h2>
-            <h3>{country.capital}</h3>
-            <div>
-              <p>
-                Right now it is{" "}
-                <strong>{parseInt(weather.main.temp)} °C </strong>
-                degrees in {country.capital} and{" "}
-                {weather.weather[0].description}.
-                </p>
-                <p>
-                  Feels like: <strong>{weather.main.feels_like}</strong>.
-                </p>
-                <Image
-                  src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-                />
-            </div>
-            <Button variant="light" onClick={() => navigate("/countries")}>
-              Back to Countries
-            </Button>
-          </Col>
-        </Row>
-      </Container>
+      <Col className="text-center m-5">
+        <Spinner
+          animation="border"
+          role="status"
+          className="center"
+          variant="info"
+        >
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      </Col>
     );
   }
 
-  // Show weather data here (minimum requirements are: Temperature, weather description and an icon)
+  return (
+    <Container fluid>
+      <Row>
+        <Col className="mt-5 d-flex justify-content-center">
+          <Image src={country.flags.svg} />
+        </Col>
+        <Col>
+          <h2>{country.name.common}</h2>
+          <h3>{country.capital}</h3>
 
-  // useEffect(() => {
-  //   if (!country) {
-  //    dispatch(initializeCountries());
-  //   }
-  // }, [location.state]);
+          <div>
+            <p>
+              Right now it is <strong>{parseInt(weather.main.temp)} </strong>
+              degrees in {country.capital} and {weather.weather[0].description}
+            </p>
+            <Image
+              src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+            />
+          </div>
+          <Button variant="light" onClick={() => navigate("/countries")}>
+            Back to Countries
+          </Button>
+        </Col>
+      </Row>
+    </Container>
+  );
+
+  // Cases we will cover together are: Country capital image
+};
 
 export default CountrySingle;
