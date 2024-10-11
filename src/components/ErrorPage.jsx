173574eb3298c37b4048c66636266ffd8acbf41b
Suyel Bishnu <suyel2007@gmail.com>
2024-10-11 17:48:07 +0300

    Style the error page

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import { Link } from "react-router-dom"; // Ensure you have react-router-dom installed
 
 const ErrorPage = () => {
   return (
-    <div>
-        This page doesn't exist 404!
-    </div>
-  )
+    <Container className="mt-5">
+      <Row className="justify-content-center text-center">
+        <Col md={6}>
+          <h1 style={{ fontSize: "5rem", color: "#dc3545" }}>404</h1>
+          <h2>This Page Doesn't Exist!</h2>
+          <p>
+            The page you are looking for might have been removed or is
+            temporarily unavailable.
+          </p>
+          <Link to="/">
+            <Button variant="primary" size="lg" className="mt-3">
+              Go Back Home
+            </Button>
+          </Link>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default ErrorPage;
