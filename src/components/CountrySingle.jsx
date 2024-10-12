aa8c16a3de36b8b32837eb788a6df266d97e54bb
Suyel Bishnu <suyel2007@gmail.com>
2024-10-12 08:58:27 +0300

    Add unsplash api to fetch images

--- src/components/CountrySingle.jsx
+++ src/components/CountrySingle.jsx
@@ -16,9 +16,11 @@ const CountrySingle = (props) => {
   const country = props.country || location.state.country;
   const [weather, setWeather] = useState("");
   const [isWeatherLoading, setIsWeatherLoading] = useState(true);
+  const [images, setImages] = useState([]); // State to store the fetched images
 
   const navigate = useNavigate();
 
+  // Fetch weather data
   useEffect(() => {
     axios
       .get(
@@ -35,7 +37,21 @@ const CountrySingle = (props) => {
       });
   }, [country.capital]);
 
-  console.log("Weather: ", weather);
+  // Fetch images from Unsplash API
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.unsplash.com/search/photos?query=${
+          country.capital
+        }&per_page=6&client_id=${import.meta.env.VITE_UNSPLASH_ACCESS_KEY}`
+      )
+      .then((response) => {
+        setImages(response.data.results); // Set the fetched images to state
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [country.capital]);
 
   if (isWeatherLoading) {
     return (
@@ -65,45 +81,52 @@ const CountrySingle = (props) => {
               style={{ maxHeight: "300px", objectFit: "contain" }}
             />
             <Card.Body>
-              <Card.Title>Country Name: {country.name.common}</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">
+              <Card.Title className="mb-4">
+                Country Name: {country.name.common}
+              </Card.Title>
+
+              <Card.Subtitle className="mb-4 text-muted">
                 <strong>Capital: </strong>
                 {country.capital}
               </Card.Subtitle>
-              <Card.Subtitle className="mb-2 text-muted">
+
+              <Card.Subtitle className="mb-4 text-muted">
                 <strong>Region: </strong>
                 {country.region}
               </Card.Subtitle>
-              <Card.Subtitle className="mb-2 text-muted">
-                <strong>Subregion:</strong> {country.subregion}
+
+              <Card.Subtitle className="mb-4 text-muted">
+                <strong>Subregion: </strong>
+                {country.subregion}
               </Card.Subtitle>
 
-              {/* Display Population */}
-              <Card.Subtitle className="mb-2 text-muted">
-                <strong>Population:</strong>{" "}
+              <Card.Subtitle className="mb-4 text-muted">
+                <strong>Population: </strong>
                 {new Intl.NumberFormat().format(country.population)}
               </Card.Subtitle>
 
-              {/* Display Area */}
-              <Card.Subtitle className="mb-2 text-muted">
-                <strong>Area:</strong>{" "}
+              <Card.Subtitle className="mb-4 text-muted">
+                <strong>Area: </strong>
                 {new Intl.NumberFormat().format(country.area)} km²
               </Card.Subtitle>
 
-              {/* Display Currency */}
-              <Card.Subtitle className="mb-2 text-muted">
-                <strong>Currency:</strong>{" "}
+              <Card.Subtitle className="mb-4 text-muted">
+                <strong>Currency: </strong>
                 {Object.values(country.currencies)
                   .map((currency) => currency.name)
                   .join(", ")}
               </Card.Subtitle>
 
-              {/* Display Official Languages */}
-              <Card.Subtitle className="mb-2 text-muted">
-                <strong>Languages:</strong>{" "}
+              <Card.Subtitle className="mb-4 text-muted">
+                <strong>Languages: </strong>
                 {Object.values(country.languages).join(", ")}
               </Card.Subtitle>
-              <Button variant="primary" onClick={() => navigate("/countries")}>
+
+              <Button
+                variant="primary"
+                onClick={() => navigate("/countries")}
+                style={{ marginTop: "20px" }} // Adding 20px space above the button
+              >
                 Back to Countries
               </Button>
             </Card.Body>
@@ -112,23 +135,57 @@ const CountrySingle = (props) => {
 
         {/* Weather Information */}
         <Col md={6}>
-          <h3>Weather in {country.capital}</h3>
-          {weather ? (
-            <div>
-              <p>
-                Currently, it is{" "}
-                <strong>{parseInt(weather.main.temp)}°C</strong> with{" "}
-                {weather.weather[0].description} in {country.capital}.
-              </p>
-              <Image
-                src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-                alt="Weather Icon"
-                style={{ width: "80px", height: "80px" }}
-              />
-            </div>
-          ) : (
-            <div>No weather data available.</div>
-          )}
+          <Card className="shadow-sm border-0 mb-3">
+            <Card.Body>
+              <h3 className="text-primary mb-3">
+                Weather in {country.capital}
+              </h3>
+
+              {weather ? (
+                <div className="text-center mb-2">
+                  <p className="lead">
+                    Currently, it is{" "}
+                    <strong>{parseInt(weather.main.temp)}°C</strong> with{" "}
+                    <strong>{weather.weather[0].description}</strong> in{" "}
+                    <strong>{country.capital}</strong>.
+                  </p>
+                  <Image
+                    src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@4x.png`}
+                    alt="Weather Icon"
+                    style={{ width: "80px", height: "80px" }}
+                    className="mb-2"
+                  />
+                </div>
+              ) : (
+                <div className="text-center text-muted mb-3">
+                  No weather data available.
+                </div>
+              )}
+
+              {/* Display Random Images of the Capital City */}
+              <h4 className="text-secondary mb-3">
+                Explore {country.capital}, {country.name.common}
+              </h4>
+
+              <Row className="gx-3 gy-3">
+                {images.map((image) => (
+                  <Col md={4} key={image.id} className="text-center">
+                    <Image
+                      src={image.urls.small}
+                      alt={image.alt_description}
+                      style={{
+                        width: "200px",
+                        height: "200px",
+                        borderRadius: "5px",
+                        objectFit: "cover",
+                      }}
+                      className="shadow-sm"
+                    />
+                  </Col>
+                ))}
+              </Row>
+            </Card.Body>
+          </Card>
         </Col>
       </Row>
     </Container>
